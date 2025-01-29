a4fb957ab5572144f1080c1c29b79ee783d835ae
ComputerCow <48889860+StinkyMilo@users.noreply.github.com>
2025-01-29 12:10:31 -0500

    you can kill goblins!

--- dungeon.js
+++ dungeon.js
@@ -11,12 +11,16 @@ const GREEN = { r: 0, g: 255, b: 0, a: 255 };
 const RED = { r: 255, g: 0, b: 0, a: 255 };
 const BLACK = {r:0,g:0,b:0,a:255};
 const ORANGE = {r:255,g:128,b:0,a:255};
+const YELLOW = {r:255,g:255,b:0,a:255};
+const PURPLE = {r:128,g:0,b:255,a:255};
 
 const START_COLOR = BLUE;
 const MID_COLOR = WHITE;
 const END_COLOR = ORANGE;
 const PUNC_START_COLOR = GREEN;
 const PUNC_END_COLOR = RED;
+const GOBLIN_SPEED=25;
+const SWORD_SPEED=10;
 
 let boardLeds;
 let numpadLeds;
@@ -28,8 +32,39 @@ let gameState = {
     activeText:[],
     activeTextIndex:0,
     autoAdvance:false,
-    nextWordTime:-1
+    nextWordTime:-1,
+    goblins:[],
+    swords:[
+        {
+            position:0,
+            timeUntilMove:SWORD_SPEED,
+            retreating:false,
+        },
+        {
+            position:0,
+            timeUntilMove:SWORD_SPEED,
+            retreating:false,
+        },
+        {
+            position:0,
+            timeUntilMove:SWORD_SPEED,
+            retreating:false,
+        },
+        {
+            position:0,
+            timeUntilMove:SWORD_SPEED,
+            retreating:false
+        }
+    ],
+    onTextEnd:()=>{}
 }
+
+const keyRows = [
+    ["SECTION","1","2","3","4","5","6","7","8","9","0","MINUS","EQUALS","BACKSPACE"],
+    ["TAB","Q","W","E","R","T","Y","U","I","O","P","SQUARE BRACKET OPEN","SQUARE BRACKET CLOSE","BACKSLASH"],
+    ["CAPS LOCK","A","S","D","F","G","H","J","K","L","SEMICOLON","QUOTE","RETURN"],
+    ["LEFT SHIFT","Z","X","C","V","B","N","M","COMMA","DOT","FORWARD SLASH","RIGHT SHIFT"]
+];
 
 for(let key in keymap){
     reverseKeymap[keymap[key][0]] = key;
@@ -120,11 +155,12 @@ function setLedGroup(group, color) {
     }
 }
 
-function setText(text,auto=false){
+function setText(text,auto=false,onTextEnd=()=>{}){
     gameState.activeText=text.split(" ");
     gameState.activeTextIndex=0;
     gameState.state="textDisplay";
     gameState.autoAdvance=auto;
+    gameState.onTextEnd=onTextEnd;
     if(auto){
         gameState.nextWordTime=getFramesForWord(gameState.activeText[gameState.activeTextIndex]);
     }
@@ -152,6 +188,13 @@ function displayWord(){
         return;
     }
     let word = gameState.activeText[gameState.activeTextIndex].toUpperCase();
+    if(word[0]=="[" && word[word.length-1] == "]"){
+        let key = word.substring(1,word.indexOf(":")).replaceAll("_"," ").toUpperCase();
+        let colorList = word.substring(word.indexOf(":")+1,word.indexOf("]")).split(",");
+        let color = {r:parseInt(colorList[0]),g:parseInt(colorList[1]),b:parseInt(colorList[2])};
+        setLedColor(key,color);
+        return;
+    }
     let hasFirst = false;
     let keyNames = [];
     let colors = [];
@@ -218,11 +261,51 @@ kbl.addListener((e,down)=>{
                     console.log("Text finished");
                     gameState.activeText=[];
                     gameState.activeTextIndex=0;
+                    gameState.onTextEnd();
                 }
+            }
+        }
+        for(let i = 0; i < gameState.swords.length; i++){
+            let sword = gameState.swords[i];
+            if(sword.retreating){
+                continue;
+            }
+            let key = keyRows[i][sword.position];
+            let anyHit = false;
+            if(e.name==key){
+                sword.position++;
+                anyHit=true;
+            }else{
+                //Allow a skip of one key.
+                let nextKey = keyRows[i][sword.position+1];
+                if(e.name==nextKey){
+                    sword.position+=2;
+                    anyHit=true;
+                }
+            }
+            if(anyHit){
+                for(let j = gameState.goblins.length-1; j >= 0; j--){
+                    let goblin = gameState.goblins[j];
+                    let goblinPos = keyRows[goblin.row].length-1-goblin.progress;
+                    if(keyRows[goblin.row][goblinPos]==e.name || keyRows[goblin.row][goblinPos+1]==e.name){
+                        gameState.goblins.splice(j,1);
+                        sword.retreating=true;
+                        sword.position=goblinPos-1;
+                    }
+                }
+                break;
             }
         }
     }
 })
+
+function newGoblin(){
+    gameState.goblins.push({
+        row:Math.floor(Math.random()*4),
+        progress:0,
+        timeUntilMove:GOBLIN_SPEED
+    });
+}
 
 let mainRunning = false;
 function main() {
@@ -232,10 +315,21 @@ function main() {
     mainRunning = true;
     getAvailableLeds();
     let allLeds = Object.values(leds);
-    setText("Hello brave adventurer. The goblins are attacking! Here comes one now.",true);
+    setText("Hello brave adventurer. The goblins are attacking! Here comes one now.",false,()=>{
+        gameState.goblins.push({
+            row:1,
+            progress:0,
+            timeUntilMove:GOBLIN_SPEED
+        });
+        setText("Stop! Whatever you do, don't press ENTER. SLASH the goblin instead. Start here, [CAPS_LOCK:255,0,0] then slide right until you reach the goblin.")
+    });
+    newGoblin();
+    newGoblin();
     let loop = setInterval(()=>{
         setLedGroup(allLeds,BLACK)
-        displayWord();
+        // displayWord();
+
+        //Update text
         if(gameState.autoAdvance){
             gameState.nextWordTime--;
             if(gameState.nextWordTime<=0){
@@ -243,6 +337,7 @@ function main() {
                 if(gameState.activeTextIndex >= gameState.activeText.length){
                     gameState.activeTextIndex=0;
                     gameState.activeText=[];
+                    gameState.onTextEnd();
                 }else{
                     gameState.nextWordTime = getFramesForWord(gameState.activeText[gameState.activeTextIndex]);
                 }
@@ -250,6 +345,39 @@ function main() {
         }else{
             setLedColor("LEFT ARROW",START_COLOR);
             setLedColor("RIGHT ARROW",END_COLOR);
+        }
+
+        //Update goblins
+        for(let i = 0; i < gameState.goblins.length; i++){
+            let goblin = gameState.goblins[i];
+            let row = keyRows[goblin.row];
+            let key = row[row.length-goblin.progress-1];
+            goblin.timeUntilMove--;
+            if(goblin.timeUntilMove <= 0){
+                goblin.progress++;
+                goblin.timeUntilMove = GOBLIN_SPEED;
+                if(goblin.progress >= row.length){
+                    //Game over
+                }
+            }
+            setLedColor(key,GREEN);
+        }
+
+        //Update Swords
+        for(let i = 0; i < gameState.swords.length; i++){
+            let sword = gameState.swords[i];
+            setLedColor(keyRows[i][sword.position],sword.position==0?PURPLE:YELLOW);
+            sword.timeUntilMove--;
+            if(sword.timeUntilMove<=0){
+                if(sword.position > 0){
+                    sword.retreating=true;
+                    sword.position--;
+                    if(sword.position==0){
+                        sword.retreating=false;
+                    }
+                }
+                sword.timeUntilMove=SWORD_SPEED;
+            }
         }
         updateLeds();
     },33);
@@ -261,3 +389,11 @@ sdk.CorsairConnect(evt => {
         main();
     }
 });
+
+/*
+    You have swords on each of the rows of keys
+    Press each key in order on that row to advance the sword.
+    Run the sword up to the goblin to kill the goblin. Then the sword must rest.
+    If you accidentally press a key a goblin is on without killing it with a sword, you lose.
+    If a goblin makes it to the end of the row, you lose.
+*/
---
+++ test.js
@@ -0,0 +1,3 @@
+let a = {a: 0};
+a.a=a;
+console.log(a);
