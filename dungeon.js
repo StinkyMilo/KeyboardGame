612ae865753b601e52b640294dd3222514e74918
ComputerCow <48889860+StinkyMilo@users.noreply.github.com>
2025-01-29 13:57:35 -0500

    goblin game!

--- dungeon.js
+++ dungeon.js
@@ -21,6 +21,8 @@ const PUNC_START_COLOR = GREEN;
 const PUNC_END_COLOR = RED;
 const GOBLIN_SPEED=25;
 const SWORD_SPEED=10;
+const MIN_GOBLIN_SPAWN = 60;
+const MAX_GOBLIN_SPAWN = 70;
 
 let boardLeds;
 let numpadLeds;
@@ -28,7 +30,7 @@ let numpadLeds;
 let leds = {};
 let device;
 let gameState = {
-    state:"textDisplay",
+    state:"intro",
     activeText:[],
     activeTextIndex:0,
     autoAdvance:false,
@@ -56,6 +58,10 @@ let gameState = {
             retreating:false
         }
     ],
+    explosions:[],
+    score:0,
+    lives:3,
+    goblinSpawnTimer:0,
     onTextEnd:()=>{}
 }
 
@@ -65,6 +71,8 @@ const keyRows = [
     ["CAPS LOCK","A","S","D","F","G","H","J","K","L","SEMICOLON","QUOTE","RETURN"],
     ["LEFT SHIFT","Z","X","C","V","B","N","M","COMMA","DOT","FORWARD SLASH","RIGHT SHIFT"]
 ];
+
+const livesKeys = ["F1", "F2", "F3"];
 
 for(let key in keymap){
     reverseKeymap[keymap[key][0]] = key;
@@ -100,6 +108,23 @@ function ledId(keyName) {
     }
     //Ignore possibility of multiple keys for now.
     return keymap[keyName][0];
+}
+
+function posOf(keyName){
+    let led = leds[ledId(keyName)];
+    return {x:led.x,y:led.y};
+}
+
+function getKeysInDistance(keyName,maxDist){
+    let pos = posOf(keyName);
+    let output = [];
+    for(const id in leds){
+        let distance = Math.sqrt((leds[id].x - pos.x)**2 + (leds[id].y-pos.y)**2);
+        if(distance <= maxDist){
+            output.push(leds[id]);
+        }
+    }
+    return output;
 }
 
 function setLedColor(keyname, color) {
@@ -158,7 +183,7 @@ function setLedGroup(group, color) {
 function setText(text,auto=false,onTextEnd=()=>{}){
     gameState.activeText=text.split(" ");
     gameState.activeTextIndex=0;
-    gameState.state="textDisplay";
+    // gameState.state="textDisplay";
     gameState.autoAdvance=auto;
     gameState.onTextEnd=onTextEnd;
     if(auto){
@@ -262,6 +287,7 @@ kbl.addListener((e,down)=>{
                     gameState.activeText=[];
                     gameState.activeTextIndex=0;
                     gameState.onTextEnd();
+                    gameState.onTextEnd=()=>{};
                 }
             }
         }
@@ -289,6 +315,20 @@ kbl.addListener((e,down)=>{
                     let goblinPos = keyRows[goblin.row].length-1-goblin.progress;
                     if(keyRows[goblin.row][goblinPos]==e.name || keyRows[goblin.row][goblinPos+1]==e.name){
                         gameState.goblins.splice(j,1);
+                        gameState.explosions.push({
+                            keys:getKeysInDistance(keyRows[goblin.row][goblinPos],20),
+                            timeLeft:6
+                        });
+                        if(sword.position <= 4){
+                            setText("GOOD",true);
+                            gameState.score++;
+                        }else if(sword.position <= 8){
+                            setText("AWESOME",true);
+                            gameState.score+=3;
+                        }else{
+                            setText("EPIC GOBLIN KILL",true);
+                            gameState.score+=5;
+                        }
                         sword.retreating=true;
                         sword.position=goblinPos-1;
                     }
@@ -296,15 +336,35 @@ kbl.addListener((e,down)=>{
                 break;
             }
         }
+        // for(let i = 0; i < gameState.goblins.length; i++){
+        //     let goblin = gameState.goblins[i];
+        //     let goblinPos = keyRows[goblin.row].length-1-goblin.progress;
+        //     let key = keyRows[goblin.row][goblinPos];
+        //     if(e.name==key){
+        //         //Game Over
+        //         gameState.lives--;
+        //     }
+        // }
     }
 })
 
 function newGoblin(){
+    let options=[0,1,2,3];
+    for(let i = 0; i < gameState.goblins.length; i++){
+        if(gameState.goblins[i].progress==0){
+            options[gameState.goblins[i].row]=-1;
+        }
+    }
+    options = options.filter((option)=>option!=-1);
+    if(options.length==0){
+        return false;
+    }
     gameState.goblins.push({
-        row:Math.floor(Math.random()*4),
+        row:options[Math.floor(Math.random()*options.length)],
         progress:0,
         timeUntilMove:GOBLIN_SPEED
     });
+    return true;
 }
 
 let mainRunning = false;
@@ -315,21 +375,26 @@ function main() {
     mainRunning = true;
     getAvailableLeds();
     let allLeds = Object.values(leds);
-    setText("Hello brave adventurer. The goblins are attacking! Here comes one now.",false,()=>{
-        gameState.goblins.push({
-            row:1,
-            progress:0,
-            timeUntilMove:GOBLIN_SPEED
-        });
-        setText("Stop! Whatever you do, don't press ENTER. SLASH the goblin instead. Start here, [CAPS_LOCK:255,0,0] then slide right until you reach the goblin.")
+    // setText("Hello brave adventurer. The goblins are attacking! Here comes one now.",false,()=>{
+    //     gameState.goblins.push({
+    //         row:1,
+    //         progress:0,
+    //         timeUntilMove:GOBLIN_SPEED
+    //     });
+    //     setText("Stop! Whatever you do, don't press ENTER. SLASH the goblin instead. Start here, [CAPS_LOCK:255,0,0] then slide right until you reach the goblin.")
+    // });
+    gameState.goblinSpawnTimer=Math.floor(Math.random()*(MAX_GOBLIN_SPAWN-MIN_GOBLIN_SPAWN))+MIN_GOBLIN_SPAWN;
+    setText("Welcome, brave adventurer!  The goblins are approaching from the East.  Grab a sword on the West side of the board and prepare for battle!  To swing your sword, place your finger on it and slide to the right. Try to make it as far right as you can. When the goblins appear, swing your swords until you hit them. The earlier you hit them, the more POINTS you'll get. Ready? Go!",false,()=>{
+        gameState.state="gameplay";
+        newGoblin();
     });
-    newGoblin();
-    newGoblin();
     let loop = setInterval(()=>{
+
+        //Clear Screen
         setLedGroup(allLeds,BLACK)
-        // displayWord();
 
         //Update text
+        displayWord();
         if(gameState.autoAdvance){
             gameState.nextWordTime--;
             if(gameState.nextWordTime<=0){
@@ -338,6 +403,7 @@ function main() {
                     gameState.activeTextIndex=0;
                     gameState.activeText=[];
                     gameState.onTextEnd();
+                    gameState.onTextEnd=()=>{};
                 }else{
                     gameState.nextWordTime = getFramesForWord(gameState.activeText[gameState.activeTextIndex]);
                 }
@@ -347,20 +413,77 @@ function main() {
             setLedColor("RIGHT ARROW",END_COLOR);
         }
 
-        //Update goblins
-        for(let i = 0; i < gameState.goblins.length; i++){
-            let goblin = gameState.goblins[i];
-            let row = keyRows[goblin.row];
-            let key = row[row.length-goblin.progress-1];
-            goblin.timeUntilMove--;
-            if(goblin.timeUntilMove <= 0){
-                goblin.progress++;
-                goblin.timeUntilMove = GOBLIN_SPEED;
-                if(goblin.progress >= row.length){
-                    //Game over
+        if(gameState.state=="gameplay"){
+            //Update goblins
+            for(let i = gameState.goblins.length-1; i >= 0; i--){
+                let goblin = gameState.goblins[i];
+                let row = keyRows[goblin.row];
+                let key = row[row.length-goblin.progress-1];
+                goblin.timeUntilMove--;
+                if(goblin.timeUntilMove <= 0){
+                    goblin.progress++;
+                    goblin.timeUntilMove = GOBLIN_SPEED;
+                    if(goblin.progress >= row.length){
+                        //Game over
+                        gameState.lives--;
+                        setText("Life Lost!",true);
+                        gameState.explosions.push({
+                            keys:getKeysInDistance(keyRows[goblin.row][0],20),
+                            timeLeft:6
+                        });
+                        if(gameState.lives<=0){
+                            gameState.goblins = [];
+                            gameState.state="gameOver";
+                            setText("Game Over. Starting Again...",false,()=>{
+                                gameState = {
+                                    state:"gameplay",
+                                    activeText:[],
+                                    activeTextIndex:0,
+                                    autoAdvance:false,
+                                    nextWordTime:-1,
+                                    goblins:[],
+                                    swords:[
+                                        {
+                                            position:0,
+                                            timeUntilMove:SWORD_SPEED,
+                                            retreating:false,
+                                        },
+                                        {
+                                            position:0,
+                                            timeUntilMove:SWORD_SPEED,
+                                            retreating:false,
+                                        },
+                                        {
+                                            position:0,
+                                            timeUntilMove:SWORD_SPEED,
+                                            retreating:false,
+                                        },
+                                        {
+                                            position:0,
+                                            timeUntilMove:SWORD_SPEED,
+                                            retreating:false
+                                        }
+                                    ],
+                                    explosions:[],
+                                    score:0,
+                                    lives:3,
+                                    goblinSpawnTimer:0,
+                                    onTextEnd:()=>{}
+                                }
+                                newGoblin();
+                                gameState.goblinSpawnTimer=Math.floor(Math.random()*(MAX_GOBLIN_SPAWN-MIN_GOBLIN_SPAWN))+MIN_GOBLIN_SPAWN;
+                            });
+                        }
+                        gameState.goblins.splice(i,1);
+                    }
                 }
+                setLedColor(key,GREEN);
             }
-            setLedColor(key,GREEN);
+            gameState.goblinSpawnTimer--;
+            if(gameState.goblinSpawnTimer<=0){
+                newGoblin();
+                gameState.goblinSpawnTimer=Math.floor(Math.random()*(MAX_GOBLIN_SPAWN-MIN_GOBLIN_SPAWN))+MIN_GOBLIN_SPAWN;
+            }   
         }
 
         //Update Swords
@@ -372,13 +495,43 @@ function main() {
                 if(sword.position > 0){
                     sword.retreating=true;
                     sword.position--;
-                    if(sword.position==0){
-                        sword.retreating=false;
-                    }
                 }
                 sword.timeUntilMove=SWORD_SPEED;
             }
+            if(sword.position==0){
+                sword.retreating=false;
+            }
         }
+
+        //Update explosions
+        for(let i = gameState.explosions.length-1; i >= 0; i--){
+            let explosion = gameState.explosions[i];
+            setLedGroup(explosion.keys,YELLOW);
+            explosion.timeLeft--;
+            if(explosion.timeLeft<=0){
+                gameState.explosions.splice(i,1);
+            }
+        }
+
+        //Update Lives
+        for(let i = 0; i < gameState.lives; i++){
+            setLedColor(livesKeys[i],RED);
+        }
+
+        //Update Score
+        let scoreStr = `${gameState.score}`;
+        let first = scoreStr[0];
+        setLedColor(`NUMPAD ${first}`,BLUE);
+        if(scoreStr.length > 1){
+            let last = scoreStr[scoreStr.length-1];
+            setLedColor(`NUMPAD ${last}`,ORANGE);
+            if(scoreStr.length > 2){
+                for(let i = 1; i < scoreStr.length-1; i++){
+                    setLedColor(`NUMPAD ${scoreStr[i]}`,WHITE);
+                }
+            }
+        }
+
         updateLeds();
     },33);
 }
